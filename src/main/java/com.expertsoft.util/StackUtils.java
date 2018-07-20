4d4f9f03291ed138e35f610bc5cc0f86627d1f90
Alex Bondarev <alexbondarev@Alexs-MacBook-Pro.local>
2018-07-21 01:32:08 +0300

    added check brackets method using stack

--- src/main/java/com.expertsoft.util/StackUtils.java
+++ src/main/java/com.expertsoft.util/StackUtils.java
@@ -1,10 +1,25 @@
 package com.expertsoft.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
+import static java.util.Collections.unmodifiableMap;
+
 public class StackUtils {
+
+    private static final Map<Character, Character> BRACKETS;
+
+    static {
+        Map<Character, Character> tmp = new HashMap<>();
+        tmp.put('(', ')');
+        tmp.put('[', ']');
+        tmp.put('{', '}');
+        tmp.put('<', '>');
+        BRACKETS = unmodifiableMap(tmp);
+    }
 
     public static <T> List<T> reverse(List<T> data) {
         Stack<T> stack = new Stack<>();
@@ -17,5 +32,25 @@ public class StackUtils {
         }
 
         return result;
+    }
+
+    public static boolean checkBrackets(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if (BRACKETS.keySet().contains(c)) {
+                stack.push(c);
+            } else if (BRACKETS.values().contains(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character ch = stack.pop();
+                if (!BRACKETS.get(ch).equals(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
--- src/test/java/com/expertsoft/util/StackUtilsTest.java
+++ src/test/java/com/expertsoft/util/StackUtilsTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class StackUtilsTest {
 
@@ -16,5 +18,30 @@ public class StackUtilsTest {
         List<Character> reverseWord = StackUtils.reverse(word);
 
         assertEquals(asList('o', 'b', 'm', 'i', 'l'), reverseWord);
+    }
+
+    @Test
+    public void checkBrackets() {
+        assertFalse(StackUtils.checkBrackets("a{b(c}d)e"));
+        assertFalse(StackUtils.checkBrackets("a[b{c}d]e}"));
+        assertFalse(StackUtils.checkBrackets("a{b(c)"));
+        assertTrue(StackUtils.checkBrackets("no brackets"));
+        assertTrue(StackUtils.checkBrackets("    public static boolean checkBrackets(String s) {" +
+                "        Stack<Character> stack = new Stack<>();" +
+                "        for (Character c : s.toCharArray()) {" +
+                "            if (BRACKETS.keySet().contains(c)) {" +
+                "                stack.push(c);" +
+                "            } else if (BRACKETS.values().contains(c)) {" +
+                "                if (stack.isEmpty()) {" +
+                "                    return false;" +
+                "                }" +
+                "                Character ch = stack.pop();" +
+                "                if (!BRACKETS.get(ch).equals(c)) {" +
+                "                    return false;" +
+                "                }" +
+                "            }" +
+                "        }" +
+                "        return stack.isEmpty();" +
+                "    }"));
     }
 }
